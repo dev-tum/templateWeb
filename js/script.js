d463989d00be9caefc280c2f2c60905dd4177324
dev-tum <sophongoon@gmail.com>
2023-06-23 14:55:55 +0700

    complete template

--- js/script.js
+++ js/script.js
@@ -38,3 +38,30 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 
 };
+
+
+/* ================================ scroll reveal ====================================== */
+
+ScrollReveal({ 
+    // reset: true ,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/* ================================ typed js ====================================== */
+
+
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer","Youtuber"] ,
+    typeSpeed: 100,
+    backSpeed: 20,
+    loop: true
+
+    });
